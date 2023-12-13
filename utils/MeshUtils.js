e3aca4788eee1324003f59cdff1c9da733efebc7
jsonp-mesh <148712831+jsonp-mesh@users.noreply.github.com>
2023-12-13 13:14:14 -0500

    V1.1 (#8)
    * added eslint
    
    * added auth bypass and defi XFER
    
    * small improvements
    
    * removed amount for defiWallet send
    
    * removed logs
    
    * small improvements
    
    * fixed handleTransferFinished func
    
    
    ---------
    
    Co-authored-by: jprix <parisijason@gmail.com>

--- utils/MeshUtils.js
+++ utils/MeshUtils.js
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router';
+
 export const handleExit = (setOpenMeshModal = () => {}) => {
   setOpenMeshModal(false);
 };
 
 export const handleSuccess = () => {
   console.log('success connecting');
+};
+
+export const handleTransferFinished = () => {
+  const router = useRouter();
+  router.push('/');
 };
